459e5ffa5542f5beb84c94448400ac054ded0df2
Vicente Marçal <vicente.marcal@gmail.com>
2020-04-19 20:20:21 -0400

    Implemented endpoint to Bot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import re
 from typing import Any, Dict
 
 from dynaconf import settings
-from pprint import pprint  # noqa
 
 import requests
 
@@ -10,6 +10,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+RE_PATTERN = r"""[v|V]aga|[f|F]ree[la|lancer]|[j|J]ob|[w|W]ork|[d|D]ev|[d|D]eveloper|[d|D]esenvolvedor
+|[j|J]unior|[p|p]leno|[s|S]enior"""
+
 
 class Message(BaseModel):
     message: Dict[str, Any]
@@ -17,19 +20,22 @@ class Message(BaseModel):
 
 @app.post("/event/")
 async def read_root(message_to_read: Message):
+
+    # Set Up
     url = f"https://api.telegram.org/bot{settings.API_TOKEN}/sendMessage"
     message_to_read = message_to_read.dict()
     animation = message_to_read["message"].get("animation", False)
 
+    # Data Processing
     if not animation:
-        msg_txt = message_to_read["message"]["text"]  # noqa
+        msg_txt = message_to_read["message"]["text"]
         msg_id = message_to_read["message"]["message_id"]
         chat_id = message_to_read["message"]["chat"]["id"]
+        if re.search(RE_PATTERN, msg_txt):
+            params = {
+                "chat_id": chat_id,
+                "text": "A vaga oferecida aceita trabalho remoto? \n# Msg enviada pelo Riverbot!",
+                "reply_to_message": msg_id,
+            }
 
-        params = {
-            "chat_id": chat_id,
-            "text": "A vaga oferecida aceita trabalho remoto? \n# Msg enviada pelo Riverbot!",
-            "reply_to_message": msg_id,
-        }
-
-        requests.post(url, params=params)
+            requests.post(url, params=params)
