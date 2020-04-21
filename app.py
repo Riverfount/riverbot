de23a87d2bfa1305fe90084e7657ac87aead9f52
Vicente Marçal <vicente.marcal@gmail.com>
2020-04-21 18:58:20 -0400

    Improved REGEX used to filter messages - Close #10

--- app.py
+++ app.py
@@ -1,39 +1,47 @@
-import logging
 import re
 
-import requests
 from dynaconf import settings
 from flask import Flask, request
+import requests
 
 
 app = Flask(__name__)
 
-RE_PATTERN_IN = r"""[v|V]aga$|[f|F]ree[la|lancer]|[j|J]ob$|[w|W]ork$|[d|D]ev|[d|D]eveloper$|[d|D]esenvolvedor$
-|[j|J]unior$|[p|P]leno$|[s|S]enior$"""
+RE_PATTERN_IN = re.compile(
+    r"""\bvagas?\b|\bempregos?\b|\bworks?\b|\bfreelancers?\b|\bfreelas?\b|\bjunior\b|\bsenior\b|\bpleno\b|\boportunidades?\b|
+\bdesenvolvedor|\bdevelopers?\b|\bdev\b""",
+    re.IGNORECASE,
+)
 
-RE_PATTERN_OUT = r"""[f|F]ake|[p|P]resnecial|[r|R]emoto"""
-
-logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+RE_PATTERN_OUT = re.compile(r"\bfake|\bpresencial|\bremot(a|o)?|\bremote", re.IGNORECASE)
 
 
 @app.route("/event/", methods=["POST"])
 def reply_message():
 
-    # Set Up
-    url = f"https://api.telegram.org/bot{settings.API_TOKEN}/sendMessage"  # noqa
+    url = f"https://api.telegram.org/bot{settings.API_TOKEN}/sendMessage"
     msg_telegram = request.get_json()
-    msg = msg_telegram.get("message", False)
 
-    if msg and not msg.get("animation", False) and not msg.get("photo", False):
-        if re.search(RE_PATTERN_OUT, msg["text"]):
-            return {"message": "out"}
-        if re.search(RE_PATTERN_IN, msg["text"]):
-            params = {
-                "chat_id": msg["chat"]["id"],
-                "text": "A vaga oferecida aceita trabalho remoto? \n# Msg enviada pelo ࿇RɨʋɛʀBօȶ࿇!",
-                "reply_to_message_id": msg["message_id"],
-            }
+    if "message" not in msg_telegram.keys():
+        return {"message": "not found!"}
 
-            requests.post(url, params=params)
+    msg = msg_telegram["message"]
+
+    if "text" not in msg.keys():
+        return {"message": "not found!"}
+
+    params = {}
+
+    if re.findall(RE_PATTERN_OUT, msg["text"]):
+        return {"message": "not found!"}
+
+    if re.findall(RE_PATTERN_IN, msg["text"]):
+        params = {
+            "chat_id": msg["chat"]["id"],
+            "text": "A vaga oferecida aceita trabalho remoto? \n# Msg enviada pelo ࿇RɨʋɛʀBօȶ࿇!",
+            "reply_to_message_id": msg["message_id"],
+        }
+
+    requests.post(url, params=params)
 
     return {"message": "complet job!"}
